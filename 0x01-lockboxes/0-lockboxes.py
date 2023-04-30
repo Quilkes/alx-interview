fa5b5272f3cef74610cc08b3961438bcf5b78044
Ajibade Abdulmatin Adekola <107342239+Banniroy@users.noreply.github.com>
2023-04-30 13:00:35 +0100

    Latest commit

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,25 +1,16 @@
 #!/usr/bin/python3
 """determine if all the boxes can be opened"""
 
-
 def canUnlockAll(boxes):
+    """This function will take a list of lists and the content
+       of a list will unlock other lists
     """
-    Return: True or False
-    """
-    a = len(boxes)
-    stack = [0]
-    unlocked_box = [1] + [0] * (a - 1)
-    i = 0
 
-    if a == 0:
+    keys = [0]
+    for key in keys:
+        for boxKey in boxes[key]:
+            if boxKey not in keys and boxKey < len(boxes):
+                keys.append(boxKey)
+    if len(keys) == len(boxes):
         return True
-    while stack:
-        j = stack.pop()
-        for index in boxes[j]:
-            if index > 0 and index < a and unlocked_box[index] == 0:
-                unlocked_box[index] = 1
-                stack.append(index)
-        i = i + 1
-    if 0 in unlocked_box:
-        return False
-    return True
+    return False
